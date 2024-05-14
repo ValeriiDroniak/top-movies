f06322b7abe8ff8d3e6db702065d2d810ea0ff4f
ValeriiDroniak <droniak.valerii@gmail.com>
2024-05-14 21:20:57 +0200

    fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 
@@ -10,15 +10,17 @@ const NotFoundPage = lazy(() => import('./pages/NotFound'));
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/movies" element={<MoviesPage />} />
-				<Route path="/serials" element={<SerialsPage />} />
-				<Route path="/cartoons" element={<CartoonsPage />} />
-				<Route path="*" element={<NotFoundPage />} />
-			</Route>
-		</Routes>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<HomePage />} />
+					<Route path="/movies" element={<MoviesPage />} />
+					<Route path="/serials" element={<SerialsPage />} />
+					<Route path="/cartoons" element={<CartoonsPage />} />
+					<Route path="*" element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 }
 
